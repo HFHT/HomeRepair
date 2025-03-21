3760534031ed7d6d13fca87bf06826444a9e5b80
Art <44448694+HFHT@users.noreply.github.com>
2025-03-21 09:17:24 -0700

    title for Home table

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Paper, Stack, Table, Title } from "@mantine/core"
+import { Paper, Stack, Table, Text, Title } from "@mantine/core"
 import { useHome } from "../hooks"
 
 export function Home() {
@@ -9,6 +9,7 @@ export function Home() {
         <Paper>
             <Title ml={8} order={3}>Home Page</Title>
             <Stack p='xs' gap='xs' align='stretch' justify='center'>
+                <Text size='md'>Repair Request Web App Visits - placeholder until dashboard developed</Text>
                 <Table striped highlightOnHover withTableBorder withColumnBorders data={tableData} />
             </Stack>
         </Paper>
